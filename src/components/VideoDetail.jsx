6f168f7a7cc2ee7fe08e2674ee6d6128570d6445
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-11 20:14:32 +0530

    modify files

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -10,9 +10,18 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 
 const VideoDetail = () => {
+
+    const [videoDetail, setVideoDetail] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+        .then((data) => setVideoDetail(data.items[0]));
+    }, [id]);
+
+
   return (
-    <div>
-      <Box minHeight="95vh" >
+    <Box minHeight="95vh" >
         <Stack direction={{ xs: "column", md: "row" }} >
             <Box flex={1} >
                 <Box sx={{ width: "100%", position: "sticky", top: "86px" }} >
@@ -20,8 +29,7 @@ const VideoDetail = () => {
                 </Box>
             </Box>
         </Stack>
-      </Box>
-    </div>
+    </Box>
   )
 }
 
